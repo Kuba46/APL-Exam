d8c81f50c9ec01f1514efb700999993364ffbe6a
Kuba <artemykuba@gmail.com>
2025-02-02 21:09:43 +0300

    added 2 LINQ tasks

---
+++ Practice/CS/OrderByAscending/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> ints = new List<int> { 1, -1213, 2132, 2, 6, 2, 3, 8, 11, 1232, 453, 563, -7872 };
+
+        List<int> sorted = ints.OrderBy(n => n).ToList();
+
+        foreach (int elem in sorted)
+        {
+            Console.WriteLine(elem);
+        }
+    }
+}
